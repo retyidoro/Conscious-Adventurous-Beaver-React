f788e9302432127958f306e69dcf318493e968c8
Dorottya Ferencz <retyidoro@yahoo.com>
2023-05-29 15:02:40 +0300

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -13,9 +13,7 @@ const Home = (props) => {
       </Helmet>
       <h1>Heading</h1>
       <div className="home-container1">
-        <svg viewBox="0 0 1024 1024" className="home-icon">
-          <path d="M554 554v-256h-84v256h84zM554 726v-86h-84v86h84zM512 86q176 0 301 125t125 301-125 301-301 125-301-125-125-301 125-301 301-125z"></path>
-        </svg>
+        <img alt="image" src="/green-400h.jpeg" className="home-image" />
       </div>
     </div>
   )
